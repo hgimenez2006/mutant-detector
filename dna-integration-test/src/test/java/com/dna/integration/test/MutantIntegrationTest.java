486909054a5ea7046c56c1202b8be01d5a040072
horacio-gimenez <horacio.gimenez@xpo.com>
2021-05-16 01:53:36 -0300

    integration test improved

--- dna-integration-test/src/test/java/com/dna/integration/test/MutantIntegrationTest.java
+++ dna-integration-test/src/test/java/com/dna/integration/test/MutantIntegrationTest.java
@@ -5,13 +5,13 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.amazonaws.services.lambda.runtime.tests.EventLoader;
 import com.dna.analyzer.AnalyzerHandler;
+import org.apache.http.HttpStatus;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import java.beans.EventHandler;
-import java.io.IOException;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(MockitoJUnitRunner.class)
 public class MutantIntegrationTest {
@@ -19,16 +19,18 @@ public class MutantIntegrationTest {
     Context context;
 
     @Test
-    public void testLoadEventBridgeEvent() throws IOException {
-        // Given
-        APIGatewayProxyRequestEvent event = EventLoader.loadApiGatewayRestEvent("apigw_rest_event.json");
-        //EventHandler<ScheduledEvent, String> handler = new EventHandler<>();
+    public void mutant_event(){
+        APIGatewayProxyRequestEvent event = EventLoader.loadApiGatewayRestEvent("mutant_event.json");
         AnalyzerHandler handler = new AnalyzerHandler();
-
-        // When
         APIGatewayProxyResponseEvent response = handler.handleRequest(event, context);
-        System.out.println(response.getBody());
-        // Then
-        //assertThat(response).isEqualTo("something");
+        assertEquals(HttpStatus.SC_OK, response.getStatusCode().longValue());
+    }
+
+    @Test
+    public void human_event(){
+        APIGatewayProxyRequestEvent event = EventLoader.loadApiGatewayRestEvent("human_event.json");
+        AnalyzerHandler handler = new AnalyzerHandler();
+        APIGatewayProxyResponseEvent response = handler.handleRequest(event, context);
+        assertEquals(HttpStatus.SC_FORBIDDEN, response.getStatusCode().longValue());
     }
 }
